99549955ce297fa1b640153ffb7db90cd05b53e1
IVAN MATHEUS RIBEIRO SILVERIO <ivansilverio005@gmail.com>
2025-04-16 20:26:10 -0300

    Create main.js

---
+++ aula06/main.js
@@ -0,0 +1,24 @@
+console.log('Hello World!');
+
+let x = 10;
+let cidade = 'Itajubá';
+
+console.log(x, cidade);
+console.log(typeof(x));
+console.log(typeof(cidade));
+
+let x = 5
+let z = 5
+let k = '5'
+
+if (x=== k){
+    console.log ('São iguais!')
+}
+
+else{
+    console.log ('nem')
+}
+
+if (x ===z){
+    console.log ('São iguais')
+}
