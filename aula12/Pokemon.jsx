faa56d1654c1107c38c29d6055c29212879b78eb
IVAN MATHEUS RIBEIRO SILVERIO <ivansilverio005@gmail.com>
2025-05-28 20:33:44 -0300

    Create Pokemon.jsx

---
+++ aula12/Pokemon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pokemon({nome, imgUrl}) {
+    
+  return (
+    <div>
+        <p>{nome}</p>
+        <img src={imgUrl} alt="Um pokemon" />
+    </div>
+    )
+}
